79adaa3dc8c22561be5d74a7f5ccf294527d5a6b
vincent servio <vincentservio@gmail.com>
2019-11-29 22:01:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,8 +17,8 @@ def load_library(filepath)
   # code goes here
 end
 
-def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
-  library = load_library(file_path)
+def get_japanese_emoticon(filepath = './lib/emoticons.yml', english_emoticon)
+  library = load_library(filepath)
   if library["get_emoticon"].include?(english_emoticon) 
     library["get_emoticon"][english_emoticon]
   else

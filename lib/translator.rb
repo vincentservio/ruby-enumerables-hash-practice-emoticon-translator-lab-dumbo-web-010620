1ac79debb2f9556d535b458ce52f51f69ee683dd
vincent servio <vincentservio@gmail.com>
2019-11-29 21:27:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,10 +1,24 @@
 # require modules here
+  require "pry"
+  require "yaml"
 
-def load_library
+
+
+def load_library(filepath)
+ library = YAML.load_file(filepath)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  library.each { |meaning, emoticons|
+    result["get_meaning"][emoticons[1]] = meaning
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
+  }
+  result
+
+
   # code goes here
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(filepath = './lib/emoticons.yml', english_emoticon)
+ library = YAML.load_file(filepath)
   # code goes here
 end
 
